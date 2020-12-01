43cf913e9b9d2486e7605792c00c32d399868819
jongfeel <DeveloperFEEL@gmail.com>
2020-12-01 19:30:17 +0900

    Add 2 character toggle and 3 character toggle images. Add activator: toggle text color

---
+++ Assets/Scripts/Activator.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Activator : MonoBehaviour
+{
+    public void Active(bool value) => gameObject.SetActive(value);
+
+    public void ActiveReverse(bool value) => gameObject.SetActive(!value);
+}
